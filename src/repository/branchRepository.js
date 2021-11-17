57d3f9c277504e626649d27400a55ba66aaa6dd6
Adia Mahardika <adiamahardika.work@gmail.com>
2021-11-17 09:57:10 +0700

    branch repository

--- src/repository/branchRepository.js
+++ src/repository/branchRepository.js
@@ -51,7 +51,7 @@ export const getDetailBranch = (branch_id) => {
 export const getBranchByMerchantId = (merchant_id) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      `SELECT branch.name as branch_name, branch.address, branch.branch_number, branch.updated_at, branch.updated_by, branch.created_at, branch.created_by FROM branch WHERE merchant_id LIKE '%${merchant_id}%'`,
+      `SELECT branch.* FROM branch WHERE merchant_id LIKE '%${merchant_id}%'`,
       (error, result) => {
         if (error) {
           console.log(error);
