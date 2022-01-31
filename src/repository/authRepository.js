70ca66e20860b77a21e99d8b8e008123e58f3f6c
Adia Mahardika <adiamahardika.work@gmail.com>
2022-01-31 14:45:43 +0700

    Edit generate trx id, inv number, and user branch id

--- src/repository/authRepository.js
+++ src/repository/authRepository.js
@@ -35,7 +35,7 @@ export const registerRepository = (request) => {
 export const emailCheckRepository = (request) => {
   return new Promise((resolve, reject) => {
     connection.query(
-      `SELECT users.*, merchant.id as merchant_id, merchant.name as merchant_name, merchant.secret_key, role.name as role_name, role.level as role_level FROM users LEFT OUTER JOIN merchant ON (merchant.owner = CAST(users.id AS varchar(10))) LEFT OUTER JOIN role ON (users.role_id = CAST(role.id AS varchar(10))) WHERE email LIKE '%${request.email}%'`,
+      `SELECT users.*, merchant.id as merchant_id, merchant.name as merchant_name, merchant.secret_key, role.name as role_name, role.level as role_level FROM users LEFT OUTER JOIN merchant ON (merchant.owner = CAST(users.id AS varchar(10))) LEFT OUTER JOIN role ON (users.role_id = CAST(role.id AS varchar(10))) WHERE email = '${request.email}'`,
       (error, result) => {
         if (error) {
           console.log(error);
